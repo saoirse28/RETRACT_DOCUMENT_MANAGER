5c1489c683331222f56b1ad9c3aa5c42f32d37f3
Erwin Macalalad <31003194+saoirse28@users.noreply.github.com>
2017-09-28 19:59:22 +0800

    Add files via upload

---
+++ SetData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToolBox.SettingDS SettingDS = new ToolBox.SettingDS();
+            SettingDS.CreateLocalSetting();
+            SettingDS.InsertLocalSetting();
+            SettingDS.SaveSetting();
+        }
+    }
+}
